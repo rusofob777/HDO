9c5498557c38ecb7eaa5e805142d1bb1253f5323
rusofob777 <telnovmax87@gmail.com>
2024-12-04 12:34:23 +0200

    l

--- src/main/java/Hivens/hdo/Registry/BlockRegistry.java
+++ src/main/java/Hivens/hdo/Registry/BlockRegistry.java
@@ -1,10 +1,12 @@
 package Hivens.hdo.Registry;
 
 import Hivens.hdo.HDO;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -19,9 +21,8 @@ public class BlockRegistry {
             DeferredRegister.create(ForgeRegistries.BLOCKS, HDO.MODID);
 
     public static final RegistryObject<Block> ATLANTUM_ORE = registerBlock("atlantum_ore",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.GOLD_ORE)
-                    .sound(SoundType.AMETHYST))
-    );
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.GOLD_ORE).strength(4)
+                    .requiresCorrectToolForDrops(), UniformInt.of(4,5)));
 
     public static final RegistryObject<Block> ATLANTUM_BLOCK = registerBlock("atlantum_block",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.GOLD_BLOCK)
